7cf986c0ef852126f2c5cfa58514fa0426117f7b
davidbermudez <davidbermudez@jerez.es>
2020-11-15 02:39:20 +0100

    Tanda 2. Ejercicios del 1 al 9

---
+++ Tanda-2/Ejercicio-t2-04/04.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio T2-04</title>
+</head>
+<body>
+    <p><a href="/index.php">Inicio</a></p>
+    <p>Vamos a ampliar uno de los ejercicios de la relación anterior para considerar las 
+    horas extras. Escribe un programa que calcule el salario semanal de un trabajador 
+    teniendo en cuenta que las horas ordinarias (40 primeras horas de trabajo) se pagan 
+    a 12 euros la hora. A partir de la hora 41, se pagan a 16 euros la hora.</p>
+    <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+        <label>Escriba el número de horas semanales:
+        <input type="number" name="hours" min="1">
+        </label>
+        <input type="submit" value="Enviar" name="Enviar">
+    </form>
+    <p>
+    <?php
+        if(isset($_POST['Enviar'])){
+            $horas = $_POST['hours'];
+            echo "$horas horas: ", $horas>40 ? 480 + ($horas-40)*16 : $horas*12, " euros.";
+        }
+    ?>
+    </p>
+</body>
+</html>
--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 </head>
 
 <body>
-    <h1>DWES</h1>
-    <p>
+    <h1>DWES</h1>    
+    <h3>Tanda 1</h3>
     <ul>
         <li><a href="Ejercicio-t1-01/01-01.php">Ejercicio 1</a></li>
         <li><a href="Ejercicio-t1-02/02-01.php">Ejercicio 2</a></li>
@@ -22,9 +22,22 @@
         <li><a href="Ejercicio-t1-09/09-01.php">Ejercicio 9</a></li>
         <li><a href="Ejercicio-t1-10/10-01.php">Ejercicio 10</a></li>
         <li><a href="Ejercicio-t1-11/11-01.php">Ejercicio 11</a></li>
-
     </ul>
-    </p>
+    <h3>Tanda 2</h3>
+    <ul>
+        <li><a href="Tanda-2/Ejercicio-t2-01/01.php">Ejercicio 1</a></li>
+        <li><a href="Tanda-2/Ejercicio-t2-02/02.php">Ejercicio 2</a></li>
+        <li><a href="Tanda-2/Ejercicio-t2-03/03.php">Ejercicio 3</a></li>
+        <li><a href="Tanda-2/Ejercicio-t2-04/04.php">Ejercicio 4</a></li>
+        <li><a href="Tanda-2/Ejercicio-t2-05/05.php">Ejercicio 5</a></li>
+        <li><a href="Tanda-2/Ejercicio-t2-06/06.php">Ejercicio 6</a></li>
+        <li><a href="Tanda-2/Ejercicio-t2-07/07.php">Ejercicio 7</a></li>
+        <li><a href="Tanda-2/Ejercicio-t2-08/08.php">Ejercicio 8</a></li>
+        <li><a href="Tanda-2/Ejercicio-t2-09/09.php">Ejercicio 9</a></li>
+        <li><a href="Tanda-2/Ejercicio-t2-10/10.php">Ejercicio 10</a></li>
+        <li><a href="Tanda-2/Ejercicio-t2-11/11.php">Ejercicio 11</a></li>
+        <li><a href="Tanda-2/Ejercicio-t2-12/12.php">Ejercicio 12</a></li>
+    </ul>
 </body>
 
 </html>
