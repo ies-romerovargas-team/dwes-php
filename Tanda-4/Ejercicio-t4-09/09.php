c64da746728a1a67f3f1b5be824f0d444ceb11de
david <davidbermudez@jerez.es>
2020-11-30 21:48:25 +0100

    Intento de E15 sin exito

--- Tanda-4/Ejercicio-t4-09/09.php
+++ Tanda-4/Ejercicio-t4-09/09.php
@@ -16,18 +16,25 @@
     Se debe comprobar que inicial es menor que final y que ambos números están entre 0 y 9.
     El programa deberá colocar el número de la posición inicial en la posición final, rotando
     el resto de números para que no se pierda ninguno. Al final se debe mostrar el array resultante</p>
-  <?php if(isset($_POST['Enviar'])):  
-      $contador = $_POST['contador'] + 1;
-      $vector = isset($_POST['vector']) ? $_POST['vector'] : array();
-      $vector[] = $_POST['numero'];
-    else:
-      $contador = 0;
-      $vector = [];
-    endif;
-    if(isset($_POST["Enviar2"])):
-
-    endif;
-    if($contador==10):?>
+  <?php
+  if(isset($_POST['Enviar'])):
+    switch($_POST['Enviar']):
+      case "Enviar":
+        $contador = $_POST['contador'] + 1;
+        $vector = isset($_POST['vector']) ? $_POST['vector'] : array();
+        $vector[] = $_POST['numero'];
+      break;
+      case "Ok":
+        $vector = $_POST['vector'];
+        $contador = 10;
+      break;
+    endswitch;
+  else:
+    $contador = 0;
+    $vector = [];
+  endif;
+  
+  if($contador==10):?>
   <table border="1" cellspacing="0">
     <tr>
       <th colspan="10">Array inicial</th>
@@ -43,15 +50,9 @@
       <?php endfor;?>
     </tr>
   </table>
-  <form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
-    <input type="number" name="inicial" value="" autofocus required>
-    <input type="number" name="final" value="" required>
-    <input type="submit" name="Enviar2" value="Ok">
-  </form>
-
-
-  <?php if(isset($_POST["Enviar2"])):?>
-  <?php // comprobar inicial<final ?>
+  <br />
+  <?php if($_POST['Enviar']=="Ok"):?>
+  <?php if($_POST['inicial']<$_POST['final']):?>
   <br>
   <table border="1" cellspacing="0">
     <tr>
@@ -63,11 +64,41 @@
       <?php endfor;?>
     </tr>
     <tr>
+      <?php
+              $aux = $vector[$_POST['final']];
+              $vector[$_POST['final']] = $vector[$_POST['inicial']];        
+              $i = $_POST['final'] + 1;
+              while($i < 10):
+                $aux2 = $vector[$i];
+                $vector[$i] = $aux;
+                $aux = $aux2;
+                $i++;
+              endwhile;
+              $i = 0;
+              while($i < $_POST['final']):
+                $aux2 = $vector[$i];
+                $vector[$i] = $aux;
+                $aux = $aux2;
+                $i++; 
+              endwhile;        
+              ?>
       <?php for($i = 0; $i < 10; $i++):?>
       <th><?=$vector[$i]?></th>
-      <?php endfor;?>
+      <?php endfor;            
+            ?>
     </tr>
   </table>
+  <p><a href="<?=$_SERVER['PHP_SELF']?>">>>Volver</a></p>
+  <?php endif;?>
+  <?php else:?>
+  <form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+    <input type="number" name="inicial" value="" autofocus required>
+    <input type="number" name="final" value="" required>
+    <?php foreach($vector as $value):?>
+    <input type="hidden" name="vector[]" value="<?=$value?>">
+    <?php endforeach;?>
+    <input type="submit" name="Enviar" value="Ok">
+  </form>
   <?php endif;?>
   <?php else:?>
   <form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
@@ -76,7 +107,7 @@
     <?php foreach($vector as $value):?>
     <input type="hidden" name="vector[]" value="<?=$value?>">
     <?php endforeach;?>
-    <input type="submit" name="Enviar" value="Ok">
+    <input type="submit" name="Enviar" value="Enviar">
   </form>
   <?php endif;?>
 </body>
--- Tanda-4/Ejercicio-t4-15/15.php
+++ Tanda-4/Ejercicio-t4-15/15.php
@@ -16,75 +16,141 @@
     original como la matriz resultado, ambas con los números convenientemente alineados.</p>
   <?php
   /*
-inicial
-  0000
-  1111
-  2222
-  3333
-  
-final
-  3210
-  3210
-  3210
-  3210
 
+33 	58 	48 	15 	75 	87 	17 	98 	95 	31 	36 	90
+78 	70 	25 	87 	71 	35 	14 	29 	48 	75 	86 	73
+44 	98 	76 	18 	46 	80 	10 	11 	91 	83 	23 	93
+91 	24 	93 	49 	28 	84 	61 	47 	82 	94 	45 	19
+31 	51 	96 	95 	24 	99 	33 	62 	17 	68 	13 	10
+20 	83 	14 	47 	21 	54 	68 	69 	97 	38 	26 	38
+49 	36 	20 	34 	37 	82 	50 	33 	20 	92 	52 	72
+14 	84 	52 	62 	91 	22 	73 	77 	70 	26 	73 	83
+63 	77 	21 	20 	57 	54 	12 	64 	98 	60 	45 	85
+12 	61 	36 	42 	54 	52 	53 	87 	52 	71 	31 	98
+31 	93 	34 	98 	37 	91 	65 	42 	71 	90 	47 	73
+66 	89 	37 	98 	91 	92 	21 	34 	99 	11 	41 	98
+
+78  33 	58 	48 	15 	75 	87 	17 	98 	95 	31 	36 	
+44 	70 	25 	87 	71 	35 	14 	29 	48 	75 	86 	90
+91 	98 	76 	18 	46 	80 	10 	11 	91 	83 	23 	73
+31 	24 	93 	49 	28 	84 	61 	47 	82 	94 	45 	93
+20 	51 	96 	95 	24 	99 	33 	62 	17 	68 	13 	19
+49 	83 	14 	47 	21 	54 	68 	69 	97 	38 	26 	10
+14 	36 	20 	34 	37 	82 	50 	33 	20 	92 	52 	38
+63 	84 	52 	62 	91 	22 	73 	77 	70 	26 	73 	72
+12 	77 	21 	20 	57 	54 	12 	64 	98 	60 	45 	83
+31 	61 	36 	42 	54 	52 	53 	87 	52 	71 	31 	85
+66 	93 	34 	98 	37 	91 	65 	42 	71 	90 	47 	98
+89 	37 	98 	91 	92 	21 	34 	99 	11 	41 	98 	73
   */
-  $min = PHP_INT_MAX;
-  $fila = 0;
-  $columna = 0;
-  $array = array();
-  for($i=0; $i<6; $i++):
-    for($j=0; $j<9; $j++):
-      $r = rand(100,999);      
-      while(in_array($r, $array)):
-        $r = rand(100,999); 
-      endwhile;
-      if($r < $min):
-        $min = $r;
-        $fila = $i;
-        $columna = $j;      
-      endif;
-      $array[$i][$j] = $r;
-      $color[$i][$j] = "black";
+  ?>
+  <div style="float:left; border:1px solid black;">
+    <table>
+      <?php for($i=0; $i<12; $i++):?>
+      <tr>
+        <?php for($j=0; $j<12; $j++):?>
+        <?php $array1[$i][$j] = rand(10,99);?>
+        <td><?=$array1[$i][$j]?></td>
+        <?php endfor;?>
+      </tr>
+      <?php endfor;?>
+    </table>
+  </div>
+  <div style="float:left; margin-left:20px; border:1px solid red">
+    <?php for($i=0; $i<5; $i++):
+    $fil = $i;
+    $col = $i;
+    $topColRight = 11 - $i;
+    $topFilDown = 11 - $i;
+    $topColLeft = $i;
+    $topFilUp = $i + 1;
+    for($j=$fil; $j<=$topColRight; $j++):
+      $array2[$i][] = $array1[$fil][$j];
+    endfor;
+    for($j=$col + 1; $j<=$topFilDown; $j++):
+      $array2[$i][] = $array1[$j][11-$col];
+    endfor;
+    for($j=11-$fil; $j>=$topColLeft; $j--):
+      $array2[$i][] = $array1[11-$fil][$j];
+    endfor;
+    for($j=11-$col; $j>=$topFilUp; $j--):
+      $array2[$i][] = $array1[$j][$fil];
     endfor;
   endfor;
-  // azul
-  $color[$fila][$columna] = "blue";
-  // verdes
-  // \.
-  $f = $fila;
-  $c = $columna;
-  while (($f + 1 <= 6) && ($c + 1 <= 9)):
-    $color[++$f][++$c] = "green";
-  endwhile;
-  // .\
-  $f = $fila;
-  $c = $columna;
-  while (($f - 1 >= 0) && ($c - 1 >= 0)):
-    $color[--$f][--$c] = "green";
-  endwhile;
-  // /.
-  $f = $fila;
-  $c = $columna;
-  while (($f - 1 >= 0) && ($c + 1 <= 9)):
-    $color[--$f][++$c] = "green";
-  endwhile;
-  // ./
-  $f = $fila;
-  $c = $columna;
-  while (($f + 1 <= 9) && ($c-1 >= 0)):
-    $color[++$f][--$c] = "green";
-  endwhile;
+  // ultimo caso
+  $array2[5][] = $array1[5][5];
+  $array2[5][] = $array1[5][6];
+  $array2[5][] = $array1[6][6];
+  $array2[5][] = $array1[6][5];
+  //Rotamos los 6 arrays
+  for($i=0; $i<6; $i++):
+    $aux1 = $array2[$i][0];
+    for($j=1; $j<count($array2[$i]); $j++):
+      $aux = $array2[$i][$j];
+      $array2[$i][$j] = $aux1;
+      $aux1 = $aux;
+    endfor;
+    $array2[$i][0] = $aux1;
+  endfor;
+  // montamos de nuevo la matriz
+  for($i=0; $i<5; $i++):
+    $fil=$i;
+    $col=$i;
+    $topColRight = 11 - $i;
+    $topFilDown = 11 - $i;
+    $topColLeft = $i;
+    $topFilUp = $i + 1;
+    foreach($array2[$i] as $value):
+      $array1[$fil][$col] = $value;
+      if($col<$topColRight):
+        $col++;
+      else:
+        if($fil<=$topFilDown):
+          $fil++;
+        else:
+          if($col>$topColLeft):
+            $col--;
+          else:
+            if($fil>=$topFilUp):
+              $fil--;
+            endif;
+          endif;
+        endif;
+      endif;
+    endforeach;
+  endfor;
+  
+  for($i=0; $i<5; $i++):
+    $fil = $i;
+    $col = $i;
+    $topColRight = 11 - $i;
+    $topFilDown = 11 - $i;
+    $topColLeft = $i;
+    $topFilUp = $i + 1;
+    for($j=$fil; $j<=$topColRight; $j++):
+      $array2[$i][] = $array1[$fil][$j];      
+    endfor;
+    for($j=$col + 1; $j<=$topFilDown; $j++):
+      $array2[$i][] = $array1[$j][11-$col];
+    endfor;
+    for($j=11-$fil; $j>=$topColLeft; $j--):
+      $array2[$i][] = $array1[11-$fil][$j];
+    endfor;
+    for($j=11-$col; $j>=$topFilUp; $j--):
+      $array2[$i][] = $array1[$j][$fil];
+    endfor;
+  endfor;
   ?>
-  <table>
-    <?php for($i=0; $i<6; $i++):?>
-    <tr>
-      <?php for($j=0; $j<9; $j++):?>
-      <td style="color: <?=$color[$i][$j]?>"><?=$array[$i][$j]?></td>
+    <table>
+      <?php for($i=0; $i<12; $i++):?>
+      <tr>
+        <?php for($j=0; $j<12; $j++):?>
+        <td><?=$array1[$i][$j]?></td>
+        <?php endfor;?>
+      </tr>
       <?php endfor;?>
-    </tr>
-    <?php endfor;?>
-  </table>
+    </table>
+  </div>
 </body>
 
 </html>
