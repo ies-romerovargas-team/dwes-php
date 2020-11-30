779f91e6e6aab221c3926b41d843a29a5b249281
david <davidbermudez@jerez.es>
2020-11-30 15:02:09 +0100

    Ajustes en ejercicios T4

--- Tanda-4/Ejercicio-t4-13/13.php
+++ Tanda-4/Ejercicio-t4-13/13.php
@@ -10,17 +10,17 @@
 <body>
   <p><a href="../../index.php">Inicio</a></p>
   <h1>Ejercicio T4-13</h1>
-  <p>Rellena un array bidimensional de 6 filas por 9 columnas con números enteros positivos 
-  comprendidos entre 100 y 999 (ambos inclusive). Todos los números deben ser distintos, es decir, 
-  no se puede repetir ninguno. Muestra a continuación por pantalla el contenido del array de 
-  tal forma que se cumplan los siguientes requisitos:</p>
+  <p>Rellena un array bidimensional de 6 filas por 9 columnas con números enteros positivos
+    comprendidos entre 100 y 999 (ambos inclusive). Todos los números deben ser distintos, es decir,
+    no se puede repetir ninguno. Muestra a continuación por pantalla el contenido del array de
+    tal forma que se cumplan los siguientes requisitos:</p>
   <ul>
     <li>Los números de las dos diagonales donde está el mínimo deben aparecer en color verde.</li>
     <li>El mínimo debe aparecer en color azul.</li>
     <li>El resto de números debe aparecer en color negro.</li>
   </ul>
   <?php
-  $min = PHP_INT_MAX;
+  $min = 999;
   $fila = 0;
   $columna = 0;
   $array = array();
@@ -68,13 +68,14 @@
   endwhile;
   ?>
   <table>
-  <?php for($i=0; $i<6; $i++):?>
+    <?php for($i=0; $i<6; $i++):?>
     <tr>
       <?php for($j=0; $j<9; $j++):?>
-        <td style="color: <?=$color[$i][$j]?>"><?=$array[$i][$j]?></td>
+      <td style="color: <?=$color[$i][$j]?>"><?=$array[$i][$j]?></td>
       <?php endfor;?>
     </tr>
-  <?php endfor;?>
+    <?php endfor;?>
   </table>
 </body>
+
 </html>
--- Tanda-4/Ejercicio-t4-15/15.php
+++ Tanda-4/Ejercicio-t4-15/15.php
@@ -4,22 +4,31 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Ejercicio T4-13</title>
+  <title>Ejercicio T4-15</title>
 </head>
 
 <body>
   <p><a href="../../index.php">Inicio</a></p>
-  <h1>Ejercicio T4-13</h1>
-  <p>Rellena un array bidimensional de 6 filas por 9 columnas con números enteros positivos 
-  comprendidos entre 100 y 999 (ambos inclusive). Todos los números deben ser distintos, es decir, 
-  no se puede repetir ninguno. Muestra a continuación por pantalla el contenido del array de 
-  tal forma que se cumplan los siguientes requisitos:</p>
-  <ul>
-    <li>Los números de las dos diagonales donde está el mínimo deben aparecer en color verde.</li>
-    <li>El mínimo debe aparecer en color azul.</li>
-    <li>El resto de números debe aparecer en color negro.</li>
-  </ul>
+  <h1>Ejercicio T4-15</h1>
+  <p>Realiza un programa que sea capaz de rotar todos los elementos de una matriz cuadrada una
+    posición en el sentido de las agujas del reloj. La matriz debe tener 12 filas por 12 columnas
+    y debe contener números generados al azar entre 0 y 100. Se debe mostrar tanto la matriz
+    original como la matriz resultado, ambas con los números convenientemente alineados.</p>
   <?php
+  /*
+inicial
+  0000
+  1111
+  2222
+  3333
+  
+final
+  3210
+  3210
+  3210
+  3210
+
+  */
   $min = PHP_INT_MAX;
   $fila = 0;
   $columna = 0;
@@ -68,13 +77,14 @@
   endwhile;
   ?>
   <table>
-  <?php for($i=0; $i<6; $i++):?>
+    <?php for($i=0; $i<6; $i++):?>
     <tr>
       <?php for($j=0; $j<9; $j++):?>
-        <td style="color: <?=$color[$i][$j]?>"><?=$array[$i][$j]?></td>
+      <td style="color: <?=$color[$i][$j]?>"><?=$array[$i][$j]?></td>
       <?php endfor;?>
     </tr>
-  <?php endfor;?>
+    <?php endfor;?>
   </table>
 </body>
+
 </html>
